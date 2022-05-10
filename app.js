5cae4888d7f51743c19b80b66a615bc83f7ec4bd
Mikaellpc <mikaellpc4@gmail.com>
2022-05-10 02:32:36 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,47 @@
+// O nodemon reinica e aplica alterações feitas no programa automaticamente
+// Normalmente é necessario reiniciar o servidor para ver as mudanças
+
+const express = require('express'); //express é responsavel por iniciar o server pelo NodeJS
+const path = require('path');
+const mysql = require('mysql'); 
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env'});
+
+const app = express(); //inicia o servidor
+//Em caso de servidor web alterar isso
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+});
+
+const publicDirectory = path.join(__dirname, './public'); //Aonde ficara os JS e CSS da pagina
+                                //dirname = pega o acesso do diretorio atual aonde está
+app.use(express.static(publicDirectory)); //faz com que o programa use o publicDirectory como acesso
+app.set('view engine', 'hbs'); //hbs é a view engine que sera usada para exibir o HTML
+
+//Envia os dados recebidos pelo HTML via forms
+app.use(express.urlencoded({ extended: false }))
+
+//Recebe os valores em forma de JSON
+app.use(express.json())
+
+db.connect( (error) => {
+    if(error) {
+        console.log(error)
+    }else{
+        console.log("MySQL OK!")
+    }
+});
+
+//Define Routes
+app.use('/', require('./routes/pages'));
+app.use('/auth', require('./routes/auth'));
+
+app.listen(3000, () => {
+    console.log("Server on")
+    console.log("Port 3000") 
+});
---
+++ controllers/auth.js
@@ -0,0 +1,46 @@
+const mysql = require('mysql'); 
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+});
+
+exports.register = (req, res) => {
+    console.log(req.body);
+    
+    // const name = req.body.name;
+    // const email = req.body.email;
+    // const telefone = req.body.telefone;
+    // const password = req.body.password;
+    // const passwordConfirm = req.body.passwordConfirm;
+
+    const { name, lastname, email, telefone, password, passwordConfirm} = req.body;
+    
+    db.query('SELECT email FROM users WHERE email = ?', [email], async (error, results) =>{
+        if(error){
+            console.log(error);
+
+        }
+        // results é recebido em forma de array
+        if( results.length > 0 ){
+            console.log('email ja utilizado')
+            return res.render('index',{
+                message: 'Este email ja esta sendo utilizado'
+            })
+        } else if( password !== passwordConfirm ){
+            console.log('senhas não conferem')
+            return res.render('index',{
+                message: 'As senhas não conferem'
+            });
+        }
+
+        let hashedPasswords = await bcrypt.hash(password, 8);
+        console.log(hashedPasswords);
+
+    });
+
+}
